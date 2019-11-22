4315b93d0a923b2524c0ccb35b53cb8ea069df45
Aayyi <julien.lavocat@gmail.com>
2019-11-22 13:05:04 +0100

    Cleanned a bit Quote class constructor

--- src/alphavantage.js
+++ src/alphavantage.js
@@ -33,7 +33,7 @@ module.exports = class AlphaVantage {
 	async search(query) {
 		try {
 
-			if(!query || query === "")
+			if (!query || query === "")
 				throw new AVError("Invalid query");
 
 			const res = await this.makeGet("SYMBOL_SEARCH", "keywords=" + query);
@@ -76,24 +76,24 @@ module.exports = class AlphaVantage {
 
 class Quote {
 	constructor(obj, useBig) {
+
+		this.symbol = obj["01. symbol"];
+		this.volume = parseInt(obj["06. volume"]);
+
 		if (useBig) {
-			this.symbol = obj["01. symbol"];
 			this.open = new Big(obj["02. open"]);
 			this.high = new Big(obj["03. high"]);
 			this.low = new Big(obj["04. low"]);
 			this.price = new Big(obj["05. price"]);
-			this.volume = parseInt(obj["06. volume"]);
 			this.latestTradingDay = new Date(obj["07. latest trading day"]);
 			this.previousClose = new Big(obj["08. previous close"]);
 			this.change = new Big(obj["09. change"]);
 			this.changePercent = new Big(obj["10. change percent"].slice(0, -1));
 		} else {
-			this.symbol = obj["01. symbol"];
 			this.open = obj["02. open"];
 			this.high = obj["03. high"];
 			this.low = obj["04. low"];
 			this.price = obj["05. price"];
-			this.volume = parseInt(obj["06. volume"]);
 			this.latestTradingDay = new Date(obj["07. latest trading day"]);
 			this.previousClose = obj["08. previous close"];
 			this.change = obj["09. change"];
